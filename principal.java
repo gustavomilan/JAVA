a8dc6eb4656251a864ae63cb2cb0536d9c83b4de
gustavomilan <125278941+gustavomilan@users.noreply.github.com>
2023-02-13 11:47:43 -0300

    Add files via upload

--- principal.java
+++ principal.java
@@ -1,15 +1,28 @@
 package principal;
 
+import java.util.Scanner;
+
+
 public class principal {
-		public static void main(String[] args) {
-			
-			Pessoa oimc = new Pessoa(100, 1.80);
-			oimc.calcula();
-			
-			Pessoa oimc2 = new Pessoa();
-			oimc2.setAltura(1.85);
-			oimc2.setPeso(100);
-			
-			oimc.calcula();
-		}
+	public static void main(String[] args) {
+		
+		
+		//get
+		Funcionario calcula = new Funcionario("Gustavo", true, 1000, 22);
+		System.out.println("[GET] Nome: " + calcula.getNome());
+		System.out.println("[GET] O Salario Bruto é: " + calcula.getSalario());
+		System.out.println("[GET] O Salario Liquido é: "+ calcula.getSalLiquido());
+		System.out.println("******************************");
+		
+		//set
+		Funcionario calcula2 = new Funcionario();
+		calcula2.setNome("Gustavo");
+		calcula2.setPf(true);
+		calcula2.setSalario(1000);
+		calcula2.setIdade(22);
+		
+		System.out.println("[GET] Nome: " + calcula.getNome());
+		System.out.println("[SET] O Salario Bruto é " + calcula2.getSalario());
+		System.out.println("[SET] O Salario Liquido é " + calcula2.getSalLiquido());
+	}
 }
